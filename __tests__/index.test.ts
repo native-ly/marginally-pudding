5b4faad6590730d20cea432387169edd8b83975e
Jakub Biesiada <biesiadajakub@icloud.com>
2022-06-17 23:38:39 +0200

    chore: update dirs and files

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,48 +4,48 @@ import { marginally, pudding, type Size } from '../src'
 import { Space } from '../src/constants'
 
 interface TestCase {
-  readonly params: Size
-  readonly expected: FlexStyle
+  readonly size: Size
+  readonly style: FlexStyle
 }
 
 const testCases = (type: Space): TestCase[] => [
   {
-    params: [20],
-    expected: {
+    size: [20],
+    style: {
       [`${type}`]: 20,
     },
   },
   {
-    params: ['102'],
-    expected: {
+    size: ['102'],
+    style: {
       [`${type}`]: '102',
     },
   },
   {
-    params: [15, 20],
-    expected: {
+    size: [15, 20],
+    style: {
       [`${type}Vertical`]: 15,
       [`${type}Horizontal`]: 20,
     },
   },
   {
-    params: ['0', '12'],
-    expected: {
+    size: ['0', '12'],
+    style: {
       [`${type}Vertical`]: '0',
       [`${type}Horizontal`]: '12',
     },
   },
   {
-    params: [16, 33, 24],
-    expected: {
+    size: [16, 33, 24],
+    style: {
       [`${type}Top`]: 16,
       [`${type}Horizontal`]: 33,
       [`${type}Bottom`]: 24,
     },
   },
   {
-    params: [2, 1, 3, 7],
-    expected: {
+    size: [2, 1, 3, 7],
+    style: {
       [`${type}Top`]: 2,
       [`${type}Right`]: 1,
       [`${type}Bottom`]: 3,
@@ -53,8 +53,8 @@ const testCases = (type: Space): TestCase[] => [
     },
   },
   {
-    params: ['3', '3', '3', '2'],
-    expected: {
+    size: ['3', '3', '3', '2'],
+    style: {
       [`${type}Top`]: '3',
       [`${type}Right`]: '3',
       [`${type}Bottom`]: '3',
@@ -62,8 +62,8 @@ const testCases = (type: Space): TestCase[] => [
     },
   },
   {
-    params: [-1, '000000002', 1_000, 0x1234],
-    expected: {
+    size: [-1, '000000002', 1_000, 0x1234],
+    style: {
       [`${type}Top`]: -1,
       [`${type}Right`]: '000000002',
       [`${type}Bottom`]: 1000,
@@ -90,8 +90,8 @@ describe.each([
 ])('$name', ({ util, cases, styleNamePlural, styleNameSingular }) => {
   it.each(testCases(cases))(
     `should create ${styleNamePlural} for $params`,
-    ({ params, expected }) => {
-      expect(util(...params)).toEqual(expected)
+    ({ size, style }) => {
+      expect(util(...size)).toEqual(style)
     }
   )
 
